e4031908be3390eeec1c5b64f4de9063ee2aa69b
ChromaticIsobar <marcotiraboschi@hotmail.it>
2021-11-10 17:06:34 +0100

    :art: Refactor parameters
    Move parameter and listeners instantiation to own functions

--- Source/processor/Plugin.cpp
+++ Source/processor/Plugin.cpp
@@ -42,37 +42,22 @@ HyperTremoloPlugin::HyperTremoloPlugin()
 #endif
               ),
 #endif
-      valueTreeState (*this, nullptr, "HyperTremolo", { std::make_unique<juce::AudioParameterFloat> ("gain", "Gain", juce::NormalisableRange<float> (-20.0f, 20.0f, 0.01f), 0.0f, "dB"), std::make_unique<juce::AudioParameterFloat> ("mix", "Mix", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 1.0f), std::make_unique<juce::AudioParameterFloat> ("tremRate", "Rate", juce::NormalisableRange<float> (0.0f, 20.0f, 0.0001f, 0.333f), 2.5f, "Hz"), std::make_unique<juce::AudioParameterFloat> ("tremRatio", "Ratio", LogRange<float> (0.1f, 10.0f, 0.001f), 1.0f), std::make_unique<juce::AudioParameterFloat> ("tremMix", "Depth", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 1.0f), std::make_unique<juce::AudioParameterBool> ("tremZero", "Through-0", false), std::make_unique<juce::AudioParameterBool> ("tremSync", "Sync", false), std::make_unique<juce::AudioParameterFloat> ("xoverFreq", "Crossover", LogRange<float> (50.0f, 20000.0f, 0.01f), 1000.0f, "Hz"), std::make_unique<juce::AudioParameterFloat> ("xoverReson", "Resonance", LogRange<float> (0.125f, 4.0f, 0.001f), 1.0f / juce::MathConstants<float>::sqrt2), std::make_unique<juce::AudioParameterFloat> ("xoverBalance", "Balance", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 0.5f), std::make_unique<juce::AudioParameterFloat> ("xoverMix", "Harmonic", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 1.0f) }),
-      gainSetter (std::bind (&juce::dsp::Gain<float>::setGainDecibels, &gain, std::placeholders::_1)),
-      mixSetter (std::bind (&juce::dsp::DryWetMixer<float>::setWetMixProportion, &dryWet, std::placeholders::_1)),
-      tremRatioSetter (std::bind (&DualTremolo<float>::setTremoloRatio, &processor, std::placeholders::_1)),
-      tremMixSetter (std::bind (&DualTremolo<float>::setTremoloMix, &processor, std::placeholders::_1)),
-      xoverFreqSetter (std::bind (&DualTremolo<float>::setCrossoverFrequency, &processor, std::placeholders::_1)),
-      xoverResonSetter (std::bind (&DualTremolo<float>::setCrossoverResonance, &processor, std::placeholders::_1)),
-      xoverBalanceSetter (std::bind (&DualTremolo<float>::setCrossoverBalance, &processor, std::placeholders::_1)),
-      xoverMixSetter (std::bind (&DualTremolo<float>::setCrossoverMix, &processor, std::placeholders::_1)),
-      tremT0AndFSetter (
-          std::bind (&DualTremolo<float>::setTremoloRate, &processor, std::placeholders::_1),
-          std::bind (&DualTremolo<float>::setTremoloThroughZero, &processor, std::placeholders::_1),
-          std::bind (&juce::AudioProcessorValueTreeState::getRawParameterValue, &valueTreeState, std::placeholders::_1),
-          "tremRate",
-          "tremZero"),
-      tremSyncSetter (std::bind (&DualTremolo<float>::sync, &processor))
+      valueTreeState (*this, nullptr, "HyperTremolo", createParameterLayout())
 {
-    // Mixer
-    valueTreeState.addParameterListener ("gain", &gainSetter);
-    valueTreeState.addParameterListener ("mix", &mixSetter);
-    // Tremolo
-    valueTreeState.addParameterListener ("tremRate", &tremT0AndFSetter);
-    valueTreeState.addParameterListener ("tremRatio", &tremRatioSetter);
-    valueTreeState.addParameterListener ("tremSync", &tremSyncSetter);
-    valueTreeState.addParameterListener ("tremMix", &tremMixSetter);
-    valueTreeState.addParameterListener ("tremZero", &tremT0AndFSetter);
-    // Crossover
-    valueTreeState.addParameterListener ("xoverFreq", &xoverFreqSetter);
-    valueTreeState.addParameterListener ("xoverReson", &xoverResonSetter);
-    valueTreeState.addParameterListener ("xoverBalance", &xoverBalanceSetter);
-    valueTreeState.addParameterListener ("xoverMix", &xoverMixSetter);
+    initializeListeners();
+}
+
+HyperTremoloPlugin::~HyperTremoloPlugin()
+{
+    auto n = listeners.size();
+    for (int i = 0; i < n; ++i)
+        if (listeners[i] != nullptr)
+        {
+            for (int j = i + 1; j < n; ++j)
+                if (listeners[i] == listeners[j])
+                    listeners[j] = nullptr;
+            delete listeners[i];
+        }
 }
 
 //==============================================================================
@@ -233,4 +218,95 @@ void HyperTremoloPlugin::setStateInformation (const void* data, int sizeInBytes)
     if (xml.get() != nullptr)
         if (xml->hasTagName (valueTreeState.state.getType()))
             valueTreeState.replaceState (juce::ValueTree::fromXml (*xml));
+}
+
+//==============================================================================
+juce::AudioProcessorValueTreeState::ParameterLayout HyperTremoloPlugin::createParameterLayout()
+{
+    return {
+        std::make_unique<juce::AudioParameterFloat> (
+            "gain", "Gain", juce::NormalisableRange<float> (-20.0f, 20.0f, 0.01f), 0.0f, "dB"),
+        std::make_unique<juce::AudioParameterFloat> (
+            "mix", "Mix", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 1.0f),
+        std::make_unique<juce::AudioParameterFloat> (
+            "tremRate", "Rate", juce::NormalisableRange<float> (0.0f, 20.0f, 0.0001f, 0.333f), 2.5f, "Hz"),
+        std::make_unique<juce::AudioParameterFloat> (
+            "tremRatio", "Ratio", LogRange<float> (0.1f, 10.0f, 0.001f), 1.0f),
+        std::make_unique<juce::AudioParameterFloat> (
+            "tremMix", "Depth", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 1.0f),
+        std::make_unique<juce::AudioParameterBool> (
+            "tremZero", "Through-0", false),
+        std::make_unique<juce::AudioParameterBool> (
+            "tremSync", "Sync", false),
+        std::make_unique<juce::AudioParameterFloat> (
+            "xoverFreq", "Crossover", LogRange<float> (50.0f, 20000.0f, 0.01f), 1000.0f, "Hz"),
+        std::make_unique<juce::AudioParameterFloat> (
+            "xoverReson", "Resonance", LogRange<float> (0.125f, 4.0f, 0.001f), 1.0f / juce::MathConstants<float>::sqrt2),
+        std::make_unique<juce::AudioParameterFloat> (
+            "xoverBalance", "Balance", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 0.5f),
+        std::make_unique<juce::AudioParameterFloat> (
+            "xoverMix", "Harmonic", juce::NormalisableRange<float> (0.0f, 1.0f, 0.001f), 1.0f)
+    };
+}
+
+void HyperTremoloPlugin::initializeListeners()
+{
+    jassert (listeners_ids.size() == 0);
+    jassert (listeners.size() == 0);
+
+    listeners_ids.push_back ("gain");
+    listeners.push_back (new SetterListener (std::bind (
+        &juce::dsp::Gain<float>::setGainDecibels, &gain, std::placeholders::_1)));
+
+    listeners_ids.push_back ("mix");
+    listeners.push_back (new SetterListener (std::bind (
+        &juce::dsp::DryWetMixer<float>::setWetMixProportion, &dryWet, std::placeholders::_1)));
+
+    listeners_ids.push_back ("tremRate");
+    listeners_ids.push_back ("tremZero");
+    auto* t0AndF = new ThroughZeroAndFrequencySetterListener (
+        std::bind (
+            &DualTremolo<float>::setTremoloRate, &processor, std::placeholders::_1),
+        std::bind (
+            &DualTremolo<float>::setTremoloThroughZero, &processor, std::placeholders::_1),
+        std::bind (
+            &juce::AudioProcessorValueTreeState::getRawParameterValue, &valueTreeState, std::placeholders::_1),
+        "tremRate",
+        "tremZero");
+    listeners.push_back (t0AndF);
+    listeners.push_back (t0AndF);
+
+    listeners_ids.push_back ("tremSync");
+    listeners.push_back (new TremSyncSetterListener (std::bind (
+        &DualTremolo<float>::sync, &processor)));
+
+    listeners_ids.push_back ("tremRatio");
+    listeners.push_back (new SetterListener (std::bind (
+        &DualTremolo<float>::setTremoloRatio, &processor, std::placeholders::_1)));
+
+    listeners_ids.push_back ("tremMix");
+    listeners.push_back (new SetterListener (std::bind (
+        &DualTremolo<float>::setTremoloMix, &processor, std::placeholders::_1)));
+
+    listeners_ids.push_back ("xoverFreq");
+    listeners.push_back (new SetterListener (std::bind (
+        &DualTremolo<float>::setCrossoverFrequency, &processor, std::placeholders::_1)));
+
+    listeners_ids.push_back ("xoverReson");
+    listeners.push_back (new SetterListener (std::bind (
+        &DualTremolo<float>::setCrossoverResonance, &processor, std::placeholders::_1)));
+
+    listeners_ids.push_back ("xoverBalance");
+    listeners.push_back (new SetterListener (std::bind (
+        &DualTremolo<float>::setCrossoverBalance, &processor, std::placeholders::_1)));
+
+    listeners_ids.push_back ("xoverMix");
+    listeners.push_back (new SetterListener (std::bind (
+        &DualTremolo<float>::setCrossoverMix, &processor, std::placeholders::_1)));
+
+    jassert (listeners_ids.size() == listeners.size());
+
+    auto n = listeners.size();
+    for (int i = 0; i < n; ++i)
+        valueTreeState.addParameterListener (listeners_ids[i], listeners[i]);
 }
